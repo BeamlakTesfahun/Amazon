5f50567bfcfcf575e70e154983c010f628296c49
Beamlak Tesfahun <beamlaktsahilu@gamil.com>
2024-09-27 18:19:13 +0300

    added all files and directories to fix github issues (unclickable folder)

---
+++ src/Pages/Orders/Orders.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LayOut from "../../Components/LayOut/LayOut.jsx";
+
+export default function Orders() {
+  return (
+    <LayOut>
+      <div>Orders</div>
+    </LayOut>
+  );
+}

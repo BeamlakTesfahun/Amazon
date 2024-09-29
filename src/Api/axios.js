9c15c6c203c7a9a49920a3a157ecc02256b19f90
Beamlak Tesfahun <beamlaktsahilu@gamil.com>
2024-09-30 00:26:25 +0300

    added deployed url on render

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/fir-85876/us-central1/api",
-  //baseURL: "https://api-yfrtdez4ia-uc.a.run.app",
+  //baseURL: "http://127.0.0.1:5001/fir-85876/us-central1/api",
+  baseURL: "https://amazon-backend-hs6c.onrender.com",
 });
 
 export { axiosInstance };
